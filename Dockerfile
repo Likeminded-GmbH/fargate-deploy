919e902cd473dc08699a5ffa9e08c9495f8e281c
Leonard Ehrenfried <mail@leonard.io>
2020-10-20 12:46:49 +0200

    Add fargate cli code

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 # Container image that runs your code
-FROM alpine:3.10
+FROM alpine:3.12
+
+RUN apk add bash aws-cli wget libc6-compat
+RUN wget -q https://github.com/turnerlabs/fargate/releases/download/v0.9.0/ncd_linux_amd64 -O /usr/bin/fargate && chmod +x /usr/bin/fargate
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
-#!/bin/sh -l
+#!/bin/bash -e
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+echo fargate-cli: `fargate --version`
+echo aws-cli:     `aws --version`
+
+export ASSUME_ROLE=$1
+export CLUSTER=$2
+export SERVICE=$3
+export CONTAINER_IMAGE=$4
+export AWS_REGION=eu-central-1
+
+# we assume the role of the terraform user and deploy as that, the scripting puts the output into the environment variables
+eval $(aws sts assume-role --role-arn $ASSUME_ROLE --role-session-name fargate-deploy | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
+fargate service deploy --service $SERVICE --image $CONTAINER_IMAGE --cluster $CLUSTER
+
